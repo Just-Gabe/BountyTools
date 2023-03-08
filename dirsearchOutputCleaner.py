795ec2fc8750c4cfc3e99c801670f4bc291a00f0
Gabe <36567844+Just-Gabe@users.noreply.github.com>
2023-03-07 22:33:15 -0300

    Alguns erros consertados

--- dirsearchOutputCleaner.py
+++ dirsearchOutputCleaner.py
@@ -7,21 +7,21 @@ if sys.argv[1] == '-h':
 else:
     path = sys.argv[1]
 
-urls=[]
-with open(f'{path}','r') as dados:
-    for line in dados:
-        linhas = line.split()
-        for listas in linhas:
-            try:
-                z = re.match("(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})", listas)
-                urls.append(z.groups())
-            except:
-                pass
+    urls=[]
+    with open(f'{path}','r') as dados:
+        for line in dados:
+            linhas = line.split()
+            for listas in linhas:
+                try:
+                    z = re.match("(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})", listas)
+                    urls.append(z.groups())
+                except:
+                    pass
 
 
-f = open('cleaned_dirsearch_output.txt', 'a')
-for i in urls:
-    for url in i:
-        f.write(url + '\n')
-f.close() 
+    f = open('cleaned_dirsearch_output.txt', 'a')
+    for i in urls:
+        for url in i:
+            f.write(url + '\n')
+    f.close() 
 
